074187d54b4776c98ef70753af5b4b5ee2b76039
shakib3164 <nazmushakib7@gmail.com>
2026-02-23 00:19:46 +0600

    added DLD lab course page using template with dummy data

---
+++ lib/courses/lab/dld_lab.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:edumate/courses//course_details_page_template.dart';
+
+class DLDLab extends StatelessWidget {
+  const DLDLab({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return CourseTemplatePage(
+      courseName: 'Digital Logic Design Lab',
+      instructorName: 'Anika Binte Aftab',
+      overview:
+      'This is the beginning of DLD Lab course...',
+
+      assignments: const [
+        AssignmentItem(
+          title: 'Design BCD adder',
+          dueDate: '22/2/2026',
+          status: 'pending',
+        ),
+      ],
+
+      lectures: const [
+      ],
+    );
+  }
+}

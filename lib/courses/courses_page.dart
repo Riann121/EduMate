245c36cb05572d1fc6022986f7f4a7a63186d83e
shakib3164 <nazmushakib7@gmail.com>
2026-03-25 21:43:26 +0600

    [RECODE]Course page blank structure

--- lib/courses/courses_page.dart
+++ lib/courses/courses_page.dart
@@ -9,7 +9,7 @@ import 'theory/dld.dart';
 //lab course pages
 import 'lab/dld_lab.dart';
 import 'lab/electronics_lab.dart';
-import 'lab/software_development_lab.dart';
+import 'lab/sd_lab.dart';
 import 'lab/data_structures_lab.dart';
 
 class CoursesPage extends StatefulWidget {
@@ -17,6 +17,8 @@ class CoursesPage extends StatefulWidget {
   @override
   State<CoursesPage> createState() => _CoursesPageState();
 }
+
+/*
 
 class _CoursesPageState extends State<CoursesPage> {
   final List<CourseItem> _theoryCourses = [
@@ -29,11 +31,7 @@ class _CoursesPageState extends State<CoursesPage> {
   final List<CourseItem> _labCourses = [
     CourseItem('Digital Logic Design Lab', 'Anika Binte Aftab', const DLDLab()),
     CourseItem('Electronics Lab', 'SJ Hamim', const ElectronicsLab()),
-    CourseItem(
-      'Software Development Lab',
-      'Fahim Ahmed',
-      const SoftwareDevelopmentLab(),
-    ),
+    CourseItem('Software Development Lab', 'Fahim Ahmed', const SoftwareDevelopmentLab(),),
     CourseItem('Data Structures Lab', 'Siam Ansary', const DataStructuresLab()),
   ];
 
@@ -182,4 +180,68 @@ class CourseTile extends StatelessWidget {
       ),
     ),
   );
+}
+*/
+class _CoursesPageState extends State<CoursesPage> {
+  final List<CourseItem> _theoryCourses = [
+    CourseItem('Data Structures', 'Siam Ansary', const DataStructures()),
+    CourseItem('Electronics', 'SJ Hamim', const Electronics()),
+    CourseItem('Ethics', 'Shamima Nasrin', const Ethics()),
+    CourseItem('Mathematics', 'Ifte Khyrul Amin', const Mathematics()),
+    CourseItem('Digital Logic Design', 'Shabnam Hasan', const DLD()),
+  ];
+  final List<CourseItem> _labCourses = [
+    CourseItem(
+      'Software Development Lab',
+      'Fahim Ahmed',
+      const SoftwareDevelopmentLab(),
+    ),
+    CourseItem('Data Structures Lab', 'Siam Ansary', const DataStructuresLab()),
+    CourseItem('Electronics Lab', 'SJ Hamim', const ElectronicsLab()),
+    CourseItem('Digital Logic Design Lab', 'Shabnam Hasan', const DLDLab()),
+  ];
+
+  void _openCourse(CourseItem course) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) => course.page));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFFFFF),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: const BackButton(color: Colors.white),
+        centerTitle: true,
+        title: const Text(
+          'Courses',
+          style: TextStyle(fontWeight: FontWeight.w900, color: Colors.white),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Courses',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//-----------------------------------------------------
+class CourseItem {
+  final String courseName;
+  final String teacherName;
+  final Widget page;
+
+  const CourseItem(this.courseName, this.teacherName, this.page);
 }

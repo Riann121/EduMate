78ef8c76647f21467fff14a654d28f8fdb730b72
shakib3164 <nazmushakib7@gmail.com>
2026-03-27 20:08:55 +0600

    [UPDATE]Variable name in assignment item class

--- lib/courses/utility/assignment_item.dart
+++ lib/courses/utility/assignment_item.dart
@@ -1,11 +1,11 @@
 class AssignmentItem {
   final String title;
   final String dueDate;
-  final String status;
+  final String details;
 
   const AssignmentItem({
     required this.title,
     required this.dueDate,
-    required this.status,
+    required this.details,
   });
 }

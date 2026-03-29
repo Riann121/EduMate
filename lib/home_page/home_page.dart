0025a360263bc0e25c99bb15f19a33cce0de7575
Rohan Rahim <rohanrahim024@gmail.com>
2026-03-29 23:38:44 +0600

    SnackBar bgcolors updated

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -37,16 +37,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  // ---------- AppBar Widget ----------
-  // AppBar _homePageAppBar() {
-  //   return AppBar(
-  //     title: const Text(
-  //       'EduMate',
-  //       style: TextStyle(fontWeight: FontWeight.w900),
-  //     ),
-  //     centerTitle: true,
-  //   );
-  // }
   AppBar _homePageAppBar() {
     return AppBar(
       title: const Text(
@@ -65,84 +55,6 @@ class _HomePageState extends State<HomePage> {
       ],
     );
   }
-
-  // ---------- Drawer Widget ----------
-  // Drawer _drawerWidget() {
-  //   return Drawer(
-  //     backgroundColor: Theme.of(context).colorScheme.surface,
-  //     child: ListView(
-  //       padding: EdgeInsets.zero,
-  //       children: [
-  //         _drawerHeader(),
-  //         _tasksTile(),
-  //         _routineTile(),
-  //         _coursesTile(),
-  //       ],
-  //     ),
-  //   );
-  // }
-
-  // Drawer Header with logo and error handling for image loading
-  // DrawerHeader _drawerHeader() {
-  //   return DrawerHeader(
-  //     child: Center(
-  //       child: Image.asset(
-  //         'assets/logos/edumate_logo.png',
-  //         width: 80,
-  //         height: 80,
-  //         fit: BoxFit.contain,
-  //         errorBuilder: (context, error, stackTrace) {
-  //           return Icon(
-  //             Icons.broken_image_outlined,
-  //             size: 80,
-  //             color: Theme.of(context).colorScheme.primary,
-  //           );
-  //         },
-  //       ),
-  //     ),
-  //   );
-  // }
-  //
-  // //tiles for navigation in drawer
-  // ListTile _coursesTile() {
-  //   return _drawerNavigationTile(
-  //     title: 'Courses',
-  //     icon: Icons.menu_book_outlined,
-  //     routeName: '/courses',
-  //   );
-  // }
-  //
-  // ListTile _routineTile() {
-  //   return _drawerNavigationTile(
-  //     title: 'Routine',
-  //     icon: Icons.schedule_outlined,
-  //     routeName: '/routine',
-  //   );
-  // }
-  //
-  // ListTile _tasksTile() {
-  //   return _drawerNavigationTile(
-  //     title: 'Tasks',
-  //     icon: Icons.task_alt_outlined,
-  //     routeName: '/tasks',
-  //   );
-  // }
-  //
-  // //method to create drawer navigation tiles
-  // ListTile _drawerNavigationTile({
-  //   required String title,
-  //   required IconData icon,
-  //   required String routeName,
-  // }) {
-  //   return ListTile(
-  //     leading: Icon(icon),
-  //     title: Text(title, style: const TextStyle(fontWeight: FontWeight.w500)),
-  //     onTap: () {
-  //       Navigator.pop(context);
-  //       Navigator.pushNamed(context, routeName);
-  //     },
-  //   );
-  // }
 
   Drawer _drawerWidget() {
     return Drawer(

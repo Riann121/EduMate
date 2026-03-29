6a52468c565860601cf2367ddba4417bc44ab86c
shakib3164 <nazmushakib7@gmail.com>
2026-03-30 05:04:55 +0600

    [ADD]lecture detail snackbar

---
+++ lib/courses/utility/lecture_details_page.dart
@@ -0,0 +1,65 @@
+import 'package:edumate/courses/utility/assignment_item.dart';
+import 'package:flutter/material.dart';
+import 'package:edumate/courses/utility/lecture_item.dart';
+
+void showLectureDetails({
+  required BuildContext context,
+  required LectureItem lecture,
+  required VoidCallback onDelete,
+}) {
+  showModalBottomSheet(
+    context: context,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    builder: (context) {
+      return Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Lecture title
+            Text(
+              lecture.title,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+
+            // Lecture date
+            Text("Date: ${formatDate(lecture.date)}"),
+            const SizedBox(height: 40),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // DOING BUTTON
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                  ),
+                  child: const Text("Pending"),
+                ),
+
+                // FINISHED BUTTON
+                ElevatedButton(
+                  onPressed: () {
+                    onDelete(); // remove lecture
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                  ),
+                  child: const Text("Finished"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}

72472406b660d81f4aa5435d22d946ccb9eb2b0f
shakib3164 <nazmushakib7@gmail.com>
2026-03-30 05:03:53 +0600

    [UPDATE]separated lecture tile, made clickable similar to assignment tile

---
+++ lib/courses/utility/lecture_tile.dart
@@ -0,0 +1,39 @@
+import 'package:edumate/courses/utility/assignment_item.dart';
+import 'package:flutter/material.dart';
+import 'package:edumate/courses/utility/lecture_item.dart';
+
+class LectureCard extends StatelessWidget {
+  final LectureItem lecture;
+  final VoidCallback? onTap;
+
+  const LectureCard({super.key, required this.lecture, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: const Color(0xFFEFEFEF),
+      elevation: 0.5,
+      margin: const EdgeInsets.only(bottom: 12),
+      child: ListTile(
+        onTap:
+            onTap ??
+            () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    "Lecture: ${lecture.title}\nDate: ${formatDate(lecture.date)}",
+                  ),
+                ),
+              );
+            },
+        leading: const Icon(Icons.library_books_outlined, color: Colors.black),
+        title: Text(
+          lecture.title,
+          style: const TextStyle(fontWeight: FontWeight.w600),
+        ),
+        subtitle: Text("Date: ${formatDate(lecture.date)}"),
+        trailing: const Text('Upcoming', style: TextStyle(fontSize: 12)),
+      ),
+    );
+  }
+}
